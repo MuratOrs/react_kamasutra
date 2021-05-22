675cae0fa63771a71409abc8defbf6a377cf2c91
Murat Ors <webschool2015@gmail.com>
2021-05-22 17:16:46 +0200

    Верстка страницы диалогом

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,15 +2,20 @@ import React, {Component} from "react";
 import s from './Dialogs.module.css'
 
 const Dialogs = () => {
-    return(
-        <div>
-            <h2>Dialogs</h2>
-            <ul>
-                <li>Murat</li>
-                <li>Murat</li>
-                <li>Murat</li>
-                <li>Murat</li>
-            </ul>
+    return (
+        <div className={s.dialogs}>
+              <div className={s.dialogsItems}>
+                  <div className={s.dialog + ' ' + s.active}>Murat</div>
+                  <div className={s.dialog}>Timur</div>
+                  <div className={s.dialog}>Bosya</div>
+                  <div className={s.dialog}>Andy</div>
+                  <div className={s.dialog}>Alex</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi, how are you?</div>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>What do you do?</div>
+            </div>
         </div>
     )
 }
