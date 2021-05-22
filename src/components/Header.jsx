0abd4289221dca4bc86be64420fdc53b0ba1a925
Murat Ors <webschool2015@gmail.com>
2021-05-22 02:44:07 +0200

    12 - разбивка на компоненты

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <header className='header'>
+            <img src="https://cdn2.iconfinder.com/data/icons/social-media-2189/48/9-Messager-256.png" alt=""/>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Profile = () => {
+    return(
+        <div className='content'>
+            <div>
+                <img src="https://scx2.b-cdn.net/gfx/news/2019/2-nature.jpg" alt=""/>
+            </div>
+            <div className='avatar'>
+                <img src="https://cdn4.iconfinder.com/data/icons/evil-icons-user-interface/64/avatar-512.png"
+                     alt=""/>
+            </div>
+            <div>
+                My posts
+            </div>
+            <div>
+                New post
+            </div>
+            <div>post 1</div>
+            <div>post 2</div>
+        </div>
+    )
+}
+
+export default Profile
